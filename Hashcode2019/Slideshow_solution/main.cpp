234cd5d7a481a5de4e2fe69186bb05491a057d91
Khalil Daoulat <48126333+Vvoox@users.noreply.github.com>
2019-03-05 23:02:07 +0000

    Update main.cpp

--- Hashcode2019/Slideshow_solution/main.cpp
+++ Hashcode2019/Slideshow_solution/main.cpp
@@ -1,10 +1,11 @@
+//still work on it , final solution soon .
 #include <iostream>
 #include <string.h>
 
 using namespace std;
 int main(){
     int N;
-    int Tmax=0 ;
+    int Tmax ;
     cin>>N ;
     int M[N];
     char orientation[N];
@@ -23,79 +24,78 @@ int main(){
             Tmax=Mi;}
 
         for(int j=0;j<Mi;j++){
-            cin>>Tags[i][j];
+            cin>>Tags[i][j];//fill in all tags in tag's tableau;
 
         }
         }
-   int length ;
-
+ /*   for(int l ; l<N ; l++){
+        for(int k ; k<Tmax ; k++){
+        cout<<Tags[l][k];
+        }
+    cout<<endl;}*/
+     int length ;
   //  if(nV % 2 == 0){
-      length=nH + nV ;
-      int slides[length];
-        for(int i ; i<N ; i++){
-           // if (orientation[i]=='H' && orientation[i+1]=='H'){ // first attempt comp between H and H
-            for(int j=1 ; j<N; j++){
+     length= nH + nV ;
+     int slides[length][2];
+    // slides[0]=0;
+   /*  for(int q ; q<length ; q++){
+         cout<<slides[q]<<endl;}
+      //for(int i ; i<length ; i++){  slides[i]=0;}//Initialize all elements of table to 0;
+      for(int i=0;i<length;i++){
+          //slides[i]=0;
+          cout<<slides[i]<<endl;}*/
+    slides[0][0]=0;
+   int cnt=0;
+     for(int i=0 ; i<N ; i++){
 
-                    for(int k=0 ; k<Tmax ;k++){
-
-                        for(int l=0 ; l<Tmax ; l++){
-
-
-                             if(  Tags[i][k]==Tags[j][l]){
-                                 for(int h ; h<length ; h++){
-                                     slides[h]=i;
-                                     slides[h+1]=j;
-
+         if( length == 1 ){
+         slides[0][0]=0;
+         cout<<slides[0][0]<<endl;
+         break;}
+            // if (orientation[i]=='H' && orientation[i+1]=='H'){ // first attempt comp between H and H
+            for(int l=0 ; l<Tmax; l++){
+                    for(int j=0 ; j<N ;j++){
+                           for(int k=0 ; k<Tmax ; k++){
+                         //  for(int h=1 ; h<length ; h++){ // boucle for fill in slides tableau ;
+                               if( Tags[i][l]==Tags[j][k] ){
+                                     if(i!=j && i<j){
+                                             slides[cnt][0]=i;
+                                             slides[cnt][1]=j;
+                                             cnt++;
+                                         //cout<<"test" << i<<" "<< j <<endl;
+                                     }
+                                     else break;
+                                         //cout<<"there is no linking Tags between slides"<<endl;
+                                      }
                                  }
-
-
-                            }
-                             else
-                                 break;
-                        }                       
-
+                    }
             }
-
-            \
-           /* if(Tags[0][0]==Tags[1][1]){
-                cout<<"there is linking tag between image "<<endl;
             }
-            else {
-                cout<<"NO connection Tags"<<endl;
-            }*/
-        //}
-
-       //   if (orientation[i]=='H' && orientation[i+1]=='V') // first attempt comp between H and V
-     /*   cout<<length<<endl; ;
-        for(int i ; i<length ; i++){
-                cout<<slides[i]<<endl;
-
-        }*/
-
-        }
-
-    /*else
-
-        nslides=nH + (nV+1)/2;*/
-
-
-    }
-    cout<<"The collection has "<<length<<" photos"<<endl;
-    for(int i=0;i<length;i++){
-        cout<<slides[i]<<endl;
-
-
-    }
+    cout<<endl;
+   cout<<"The slideshow has "<<length<<" slides"<<endl;
+   for(int l=0 ; l<N ; l++){
+         // cout<<slides[l];
+              cout<<slides[l][0]<<"\t"<<slides[l][1];
+              cout<<endl;
 
   }
 
+}
 
 
 
+\
+/* if(Tags[0][0]==Tags[1][1]){
+    cout<<"there is linking tag between image "<<endl;
+}
+else {
+    cout<<"NO connection Tags"<<endl;
+}*/
+//}
 
+//   if (orientation[i]=='H' && orientation[i+1]=='V') // first attempt comp between H and V
+/*   cout<<length<<endl; ;
+for(int i ; i<length ; i++){
+    cout<<slides[i]<<endl;
 
-
-
-
-
-
+}*/
