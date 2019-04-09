9104d0bbbfaf52706510d31d2460257b12312b07
Khalil Daoulat <48126333+Vvoox@users.noreply.github.com>
2019-04-09 20:49:18 +0100

    Add files via upload

---
+++ Chill_Zone_Fun/src/com/example/java/Main.java
@@ -0,0 +1,77 @@
+package com.example.java;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int T , r, c;
+    //public static String symbol[][] =new String[r][c];
+    public static void main(String[] args) {
+
+        System.out.println("Hello in the Chil Zone Fun problem");
+        //System.out.print("Enter your T number of cases : ");
+        Scanner test = new Scanner(System.in);
+        T = test.nextInt();
+        //System.out.print("Enter your r the number of lines :  ");
+        Scanner line = new Scanner(System.in);
+        r = line.nextInt();
+        //System.out.print("Enter your c the number of grids : ");
+        Scanner grid = new Scanner(System.in);
+        c = grid.nextInt();
+        chill_zone_fun(T,r,c);
+    }
+
+    public static void chill_zone_fun(int T , int r , int c){
+       String symbol[][] =new String[r][c];
+        System.out.println("Symbols suggest is {'.','*','K','S','T'} : ");
+        Scanner tab=new Scanner(System.in);
+
+        for(int k=0 ; k<T ; k++){
+
+            for(int i=0 ; i<r;i++){
+
+                for(int j=0 ; j<c;j++){
+
+                    symbol[i][j]=tab.next();
+                }
+            }
+        }
+        System.out.println("Your symbol table is : ");
+        for(int k=0 ; k<T ; k++) {
+
+            for (int i = 0; i < r; i++) {
+
+                for (int j = 0; j < c; j++) {
+                    System.out.print(symbol[i][j] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+        stplace(symbol);
+    }
+
+    public static void stplace(String[] symbol[]){
+
+        int x_s=0 , y_s=0 , x_t=0, y_t=0 ;
+
+        for(int i=0 ; i<r ; i++){
+
+            for(int j=0 ; j<c ; j++){
+
+                if(symbol[i][j].equals("S")){
+                    x_s=i;
+                    y_s=j;
+                }
+                if(symbol[i][j].equals("T")){
+                    x_t=i;
+                    y_t=j;
+                }
+            }
+        }
+        System.out.println(x_s+" "+y_s+"\n"+x_t+" "+y_t);
+
+
+    }
+}
