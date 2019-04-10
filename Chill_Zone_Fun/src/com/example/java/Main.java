8d4792244a3e12bbe07908a837c3e42929383d6e
Khalil Daoulat <48126333+Vvoox@users.noreply.github.com>
2019-04-10 10:48:50 +0100

    Add files via upload

--- Chill_Zone_Fun/src/com/example/java/Main.java
+++ Chill_Zone_Fun/src/com/example/java/Main.java
@@ -4,55 +4,59 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int T , r, c;
+    //public static int T;
+    //public static int[] stdcost ;
+    public static int counter=0;
+
+
     //public static String symbol[][] =new String[r][c];
     public static void main(String[] args) {
 
         System.out.println("Hello in the Chil Zone Fun problem");
         //System.out.print("Enter your T number of cases : ");
         Scanner test = new Scanner(System.in);
-        T = test.nextInt();
+        int T = test.nextInt();
         //System.out.print("Enter your r the number of lines :  ");
-        Scanner line = new Scanner(System.in);
-        r = line.nextInt();
-        //System.out.print("Enter your c the number of grids : ");
-        Scanner grid = new Scanner(System.in);
-        c = grid.nextInt();
-        chill_zone_fun(T,r,c);
+        for (int i = 0; i < T; i++) {
+            Scanner line = new Scanner(System.in);
+            int r = line.nextInt();
+            //System.out.print("Enter your c the number of grids : ");
+            Scanner grid = new Scanner(System.in);
+            int c = grid.nextInt();
+            chill_zone_fun(T, r, c);
+            counter++;
+        }
+       /* for(int j=0;j<stdcost.length;j++){
+            System.out.println(stdcost[j]);
+        }*/
     }
 
-    public static void chill_zone_fun(int T , int r , int c){
-       String symbol[][] =new String[r][c];
+    public static void chill_zone_fun(int T, int r, int c) {
+        String symbol[][] = new String[r][c];
         System.out.println("Symbols suggest is {'.','*','K','S','T'} : ");
-        Scanner tab=new Scanner(System.in);
+        Scanner tab = new Scanner(System.in);
 
-        for(int k=0 ; k<T ; k++){
+        for (int i = 0; i < r; i++) {
 
-            for(int i=0 ; i<r;i++){
+            for (int j = 0; j < c; j++) {
 
-                for(int j=0 ; j<c;j++){
-
-                    symbol[i][j]=tab.next();
-                }
+                symbol[i][j] = tab.next();
             }
         }
-        System.out.println("Your symbol table is : ");
-        for(int k=0 ; k<T ; k++) {
+       // System.out.println("Your symbol table is : ");
 
-            for (int i = 0; i < r; i++) {
+        /*for (int i = 0; i < r; i++) {
 
-                for (int j = 0; j < c; j++) {
-                    System.out.print(symbol[i][j] + " ");
-                }
-                System.out.println();
+            for (int j = 0; j < c; j++) {
+                System.out.print(symbol[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println();*/
+    stplace(T,r, c, symbol);
 
-        stplace(symbol);
-    }
-
-    public static void stplace(String[] symbol[]){
+}
+    public static void stplace(int T ,int r , int c,String[] symbol[]){
 
         int x_s=0 , y_s=0 , x_t=0, y_t=0 ;
 
@@ -70,108 +74,75 @@ public class Main {
                 }
             }
         }
-        System.out.println(x_s+" "+y_s+"\n"+x_t+" "+y_t);
-        System.out.println(cost(x_s , y_s , x_t ,y_t,symbol));
+       // System.out.println(x_s+" "+y_s+"\n"+x_t+" "+y_t);
+        cost(T , x_s , y_s , x_t ,y_t,symbol);
     }
-    public static int cost(int x0 , int y0 , int x1 , int y1,String[] symbol[]){
-        int cost=0;
+    public static void cost(int T, int x0 , int y0 , int x1 , int y1,String[] symbol[]){
+       int cost= 0;
+       int[] stdcost =new int[T];
+        
+           if (x0 <= x1 && y0 <= y1) {
+               for (int i = x0; i <= x1; i++) {
+                   for (int j = y0; j <= y1; j++) {
 
-       /* for(int i=0 ; i<r ; i++){
+                       if (symbol[i][j].equals(".")) {
+                           cost += 1;
+                       }
+                       if (symbol[i][j].equals("K")) {
+                           cost += 0;
+                       }
+                   }
+               }
 
-            for(int j=0 ; j<c ; j++){
+           }
+           if (x0 >= x1 && y0 >= y1) {
+               for (int i = x1; i <= x0; i++) {
+                   for (int j = y1; j <= y0; j++) {
 
-                if (x0<x1 && y0<y1){
-                    while(x0<x1){
-                        cost+=1;
+                       if (symbol[i][j].equals(".")) {
+                           cost+= 1;
+                       }
+                       if (symbol[i][j].equals("K")) {
+                           cost+= 0;
+                       }
+                   }
+               }
 
-                        x0++; }
-                    while(y0<y1){
-                        cost+=1;
-                        y0++; } }
+           }
+           if (x0 <= x1 && y0 >= y1) {
+               for (int i = x0; i <= x1; i++) {
+                   for (int j = y1; j <= y0; j++) {
 
-                if (x0>x1 && y0>y1){
-                    while(x0>x1){
-                        cost+=1;
-                        x0--; }
-                    while(y0>y1){
-                        cost+=1;
-                        y0--; } }
+                       if (symbol[i][j].equals(".")) {
+                           cost+= 1;
+                       }
+                       if (symbol[i][j].equals("K")) {
+                           cost+= 0;
+                       }
+                   }
+               }
 
-                if (x0<x1 && y0>y1){
-                    while(x0<x1){
-                        cost+=1;
-                        x0++; }
-                    while(y0>y1){
-                        cost+=1;
-                    y0--; } }
+           }
+           if (x0 >= x1 && y0 <= y1) {
+               for (int i = x1; i <= x0; i++) {
+                   for (int j = y0; j <= y1; j++) {
 
-                if (x0>x1 && y0<y1){
-                    while( x0>x1){
-                        cost+=1;
-                        x0--; }
-                    while(y0<y1){
-                        cost+=1;
-                        y0++; } }
+                       if (symbol[i][j].equals(".")) {
+                           cost+= 1;
+                       }
+                       if (symbol[i][j].equals("K")) {
+                           cost+= 0;
+                       }
+                   }
+               }
 
+           }
+           else {
+               cost+= -1 ;
 
-            }
-        }
-        System.out.println("Your cost to reach the target is : ");
-        return cost;*/
-        if (x0<=x1 && y0<=y1) {
-            for (int i = x0; i <= x1; i++) {
-                for (int j = y0; j <= y1; j++) {
+           }
 
-                    if (symbol[i][j].equals(".")) {
-                        cost += 1;
-                    }
-                    if (symbol[i][j].equals("K")) {
-                        cost += 0;
-                    }
-                }
-            }
-        }
-        if (x0>=x1 && y0>=y1) {
-            for (int i = x1; i <= x0; i++) {
-                for (int j = y1; j <= y0; j++) {
-
-                    if (symbol[i][j].equals(".")) {
-                        cost += 1;
-                    }
-                    if (symbol[i][j].equals("K")) {
-                        cost += 0;
-                    }
-                }
-            }
-        }
-        if (x0<=x1 && y0>=y1) {
-            for (int i = x0; i <= x1; i++) {
-                for (int j = y1; j <= y0; j++) {
-
-                    if (symbol[i][j].equals(".")) {
-                        cost += 1;
-                    }
-                    if (symbol[i][j].equals("K")) {
-                        cost += 0;
-                    }
-                }
-            }
-        }
-        if (x0>=x1 && y0<=y1) {
-            for (int i = x1; i <= x0; i++) {
-                for (int j = y0; j <= y1; j++) {
-
-                    if (symbol[i][j].equals(".")) {
-                        cost += 1;
-                    }
-                    if (symbol[i][j].equals("K")) {
-                        cost += 0;
-                    }
-                }
-            }
-        }
-        return cost;
-
+           System.out.println(stdcost[counter]=cost);
 
     }
 }
