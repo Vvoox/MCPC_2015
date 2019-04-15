d033fb8bf8725186cde9a9955e0e22918cec5c99
Khalil Daoulat <48126333+Vvoox@users.noreply.github.com>
2019-04-15 12:20:22 +0100

    Add files via upload

--- Gimme_Another_Challenge/src/com/example/java/Main.java
+++ Gimme_Another_Challenge/src/com/example/java/Main.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static int[] solution;
+    //public static int[] tab;
+    public static int N=0;
+    //public static int[] tab = new int[]{};
     public static void main(String[] args) {
     System.out.println("Hello , we need to help in Jeff's problem to calculate the space in the array ");
-    System.out.print("Insert your test cases number :  ");
+    System.out.println("Insert your test cases number :  ");
     Scanner cs = new Scanner(System.in);
     Scanner num = new Scanner(System.in);
     Scanner tb = new Scanner(System.in);
     int T = cs.nextInt();
+
     for(int l=0 ; l<T ; l++) {
-        int N = cs.nextInt();
+        N = cs.nextInt();
         String tab1[]=new String[N];
         int tab[]=new int[N];
         for (int i = 0; i < N; i++) {
@@ -21,28 +26,30 @@ public class Main {
             tab[i] = Integer.parseInt(tab1[i]);
 
         }
-    }
-    for(int i=0 ; i<N ; i++){
 
-        System.out.print(tab1[i]+" ");
-    }
-    chall(tab);
 
+        //System.out.println();
+        chall(tab);
     }
 
-    public static void chall(int tab[]){
-            int space =0 ;
+
+    }
+
+    public static void chall(int[] tab){
+            int space =1 ;
+            int space0 = 0;
 
             for(int i=0 ; i<tab.length ; i++){
+                if(i+1 == N) break;
 
-                for(int j=i+1 ; j<tab.length ; j++){
-
-                    if(tab[i]<=tab[j]){
-                         space+=1;
-
+                if(tab[i]<=tab[i+1]){
+                    if(tab[i]==0){
+                        space=1;
+                    }
+                    else space+=1;
 
                     }
-                }
+
             }
             System.out.println(space);
 
